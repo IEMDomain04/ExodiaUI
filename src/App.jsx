8ea4ed7cc58b4a539a6e1a560d3ff43aa3580848
IEMDomain04.dc <emmanman0011@gmail.com>
2025-06-16 15:37:13 +0800

    setup: react router and Browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import Footer from './components/Footer';
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";  
 
 function App() {
-  
   return (
     <>
-
-
-     <Footer />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        
+        {/* <Route path="/aboutme" element={<text />} />
+        <Route path="/projects" element={<text />} />
+        <Route path="/casestudy" element={<text />} /> */}
+      </Routes>
     </>
   );
 }
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import Footer from '../components/Footer';
+
+function MainPage() {
+    return (
+        <main>
+            <Footer />
+        </main>
+    );
+}
+
+export default MainPage;
