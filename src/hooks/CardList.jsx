c184c66c7aedd03e9d8e21d0fc298da3b98a94b2
IEMDomain04.dc <emmanman0011@gmail.com>
2025-06-21 22:03:36 +0800

    update(CardList): add layout for the cards; Todo: apply for large scale screens as well

--- src/hooks/CardList.jsx
+++ src/hooks/CardList.jsx
@@ -1,35 +1,40 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
+
+const API_URL = "";
 
 function CardList() {
-    const [cards, setCards] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [cards, setCards] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  // Fetch cards from the API
+  useEffect(() => {
+    fetch(API_URL)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCards(data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.error("FETCHING CARDS ERROR: ", error);
+      });
+  });
 
-    // Fetch cards from the API
-    useEffect(() => {
-        fetch("")
-        .then((resp) => resp.json())
-        .then((data) => {
-            setCards(data);
-            setLoading(false);
-        })
-        .catch((error) => {
-            setLoading(false);
-            console.error("FETCHING CARDS ERROR: ", error);
-        })
-    })
+  if (loading) return <p>LOADING!!</p>;
 
-    if (loading) return <p>LOADING!!</p>
-
-    return(
-        <section>
-            <div>
-                {cards.map((card) => (
-                    <h1></h1>
-                ))}
-            </div>
-        </section>
-    );
+  return (
+    <section className="grid grid-cols-2 p-3">
+      {cards.map((card) => (
+        <div className="border border-black rounded-xl m-2 flex flex-col justify-center text-center items-center p-5">
+          <img
+            src={card.card_images[0].image_url}
+            alt={card.name}
+          />
+          <h1>{card.name}</h1>
+        </div>
+      ))}
+    </section>
+  );
 }
 
 export default CardList;
