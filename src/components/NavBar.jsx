d794e1ef343defee576b21daf49109936c22e6aa
IEMDomain04.dc <emmanman0011@gmail.com>
2025-06-21 23:22:24 +0800

    feat: add navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+function NavBar() {
+    return(
+        <nav className="bg-black border-2 border-b-white">
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+            </ul>    
+        </nav>
+    );
+}
+
+export default NavBar;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,11 @@
+import NavBar from '../components/NavBar';
 import CardList from '../hooks/CardList';
 import Footer from '../components/Footer';
 
 function MainPage() {
     return (
         <main>
+            <NavBar />
             <CardList />
             <Footer />
         </main>
