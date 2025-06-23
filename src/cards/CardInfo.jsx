8ddc5dfc8e1545acfbfba062c1fc42706d09ae52
IEMDomain04.dc <emmanman0011@gmail.com>
2025-06-23 08:05:00 +0800

    chore(cardinfo): when no data, the tag will not display

--- src/cards/CardInfo.jsx
+++ src/cards/CardInfo.jsx
@@ -31,33 +31,41 @@ function CardInfo() {
         className="bg-amber-400 text-black mx-10 px-4 py-2 rounded-md mb-10"
       >
         ← Back
-        </button>
-      
-    <section className="bg-darkColor border border-white p-5 rounded-xl m-5 text-white">  
-      {/* Whole Contents */}
-      <div className="max-w-5xl mx-auto flex items-center">
-        {/* Image of the Card */}
-        <img className="w-30 md:w-50 xl:w-70 border border-white rounded-xl" src={card.card_images[0].image_url} alt={card.name} />
+    </button>
+
+    <div className="max-w-7xl flex mx-auto items-center">
+      {/* Image of the cards */}
+      <img className="w-30 md:w-50 xl:w-70 border border-white rounded-xl" src={card.card_images[0].image_url} alt={card.name} />  
+      <div className="bg-darkColor border border-white h-fit w-fit rounded-xl p-10 mx-10 text-white">  
         {/* Text, Tags, Description */}
-        <div className="mx-10 space-y-10">
+        <div className="space-y-10">
             <div className="space-y-3">
                 <div className="flex items-end gap-5">
                     <h1 className="text-h1 font-bold tracking-widest">{card.name}</h1>
                     <h2 className="text-h3 text-blue-300 tracking-wide">{card.type}</h2>
                 </div>
                 <div className="flex gap-5">
-                    <p className="bg-attackTag rounded-full border border-white w-fit px-5 py-1">ATK: {card.atk}</p>
-                    <p className="bg-defenseTag rounded-full border border-white w-fit px-5 py-1">DEF: {card.def}</p>
-                    <p className="bg-levelTag rounded-full border border-white w-fit px-5 py-1">LVL: {card.level}</p>
-                    <p className="bg-attributeTag rounded-full border border-white w-fit px-5 py-1">{card.attribute}</p>
-                    <p className="bg-raceTag rounded-full border border-white w-fit px-5 py-1">{card.race}</p>
+                    {card.atk && (
+                      <p className="bg-attackTag rounded-full border border-white w-fit px-5 py-1">ATK: {card.atk}</p>
+                    )}
+                    {card.def && (
+                      <p className="bg-defenseTag rounded-full border border-white w-fit px-5 py-1">DEF: {card.def}</p>
+                    )}
+                    {card.level && (
+                      <p className="bg-levelTag rounded-full border border-white w-fit px-5 py-1">LVL: {card.level}</p>
+                    )}
+                    {card.attribute && (
+                      <p className="bg-attributeTag rounded-full border border-white w-fit px-5 py-1">{card.attribute}</p>
+                    )}
+                    {card.race && (
+                      <p className="bg-raceTag rounded-full border border-white w-fit px-5 py-1">{card.race}</p>
+                    )}
                 </div>
-            </div>
+              </div>
             <p className="text-h3 tracking-wide">{card.desc}</p>
+          </div>
         </div>
-      
       </div>
-    </section>
     </section>
   );
 }
