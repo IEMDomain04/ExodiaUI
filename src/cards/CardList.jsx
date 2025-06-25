196b20314cd4338e12c6b4376a55c24b4374d6e4
IEMDomain04.dc <emmanman0011@gmail.com>
2025-06-25 14:19:45 +0800

    feat: add loading screen

--- src/cards/CardList.jsx
+++ src/cards/CardList.jsx
@@ -19,7 +19,6 @@ function CardList({ searchCard, sortType, sortRace, sortAttribute }) {
       .then((resp) => resp.json())
       .then((data) => {
         setAllCards(data.data || []);
-        setLoading(false);
       })
       .catch((error) => {
         setLoading(false);
@@ -41,7 +40,19 @@ function CardList({ searchCard, sortType, sortRace, sortAttribute }) {
   // Paginated view of filtered cards
   const paginatedCards = filteredCards.slice(page * CARDS_PER_PAGE, (page + 1) * CARDS_PER_PAGE);
 
-  if (loading) return <p>LOADING!!</p>;
+  // Loading cards
+  if (loading) {
+    return (
+      <div className="flex flex-col justify-center items-center my-30 gap-4 text-white animate-pulse md:my-50">
+        <img
+          src="/assets/logo.png" // replace with your logo or an animated loader
+          alt="Loading Yu-Gi-Oh Cards"
+          className="w-50 opacity-80 md:w-[400px]"
+        />
+        <p className="text-xl font-semibold">Summoning cards...</p>
+      </div>
+    );
+  }
 
   return (
     <main>
